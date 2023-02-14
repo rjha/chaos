df1644414a3a1676695d9fd212075799130b8005
Rajeev Jha <jha.rajeev@gmail.com>
2023-02-14 17:05:48 +0530

    update mandlebrot library
    - add getVerticalPoints() and getGridPoints() method
    - add stop() condition
    - use setters for properties

--- js/mandlebrot.js
+++ js/mandlebrot.js
@@ -6,57 +6,155 @@ const MAX_ITER = 1000;
 
 class Mandlebrot {
 
-
+  
     #transformer; 
     #attractor_color = "red";
-    #colors = [
-        "black", 
-        "blue",
-        "yellow", 
-        "orange",
-        "green",
-        "aqua",
-        "teal",
-        "navy"];
-    
-    #colorIndex = [10, 50, 100];
-    
-    setColors(colors) {
+    #colors = [];
+    #colorsIndex = [];
+    #pixels = 0;   
+    #xp = 0;
+    #yp = 0;
+    #total = 0;
+
+
+    constructor(args={}) {
+
+       // default options
+       const defaults = {
+          "pixels": undefined,
+          "transformer": undefined, 
+          "colorsIndex": [40, 60, 90, 110, 150, 200, 300],
+          "colors": [
+            "black", 
+            "blue",
+            "yellow", 
+            "orange",
+            "green",
+            "aqua",
+            "teal",
+            "navy"],
+      }
+
+      const options = Object.assign(defaults, args);
+      this.#pixels = options.pixels;
+      this.#colors = options.colors;
+      this.#colorsIndex = options.colorsIndex;
+      this.#total = 0;
+
+    }
+
+   
+    set colors(colors=[]) {
       this.#colors = colors; 
     }
 
-    setColorIndex(colorIndex) {
-      this.#colorIndex = colorIndex;
+    set colorsIndex(colorsIndex=[]) {
+      this.#colorsIndex = colorsIndex;
     }
     
-    setTransformer(transformer_func) {
+    set transformer(transformer_func) {
       this.#transformer = transformer_func;
     }
 
-    render(xp) {
-      
-      let y_pixels = this.plotter.resolution.y;
-      
-      for(let yp = 0; yp < y_pixels; yp = yp + 1) {
+    stop() {
 
-        // map the pixel into a number 
-        // in complex plane 
-        let z = this.plotter.mapXY(xp, yp);
-        let orbit = this.#transformer(z);
-        let color = this.getColor(orbit.n);
-
-        this.plotter.add(['TDOT', {
-          "x": z.x, 
-          "y": z.y,
-          "color": color  
-        }]);
-
-          
+      if(this.#total >= (this.#pixels * this.#pixels)) {
+        return true;
       }
+
+      return false;
 
     }
 
     
+    getVerticalPoints() {
+      
+      let points = [];
+
+      // get all vertical points for #xp  
+      for(let yp = 0; yp < this.#pixels; yp++) {
+
+        // map the pixel into a number 
+        // in complex plane 
+        let z = this.plotter.mapXY(this.#xp, yp);
+        let orbit = this.#transformer(z);
+        let color = this.getColor(orbit.n);
+
+        points.push({
+            "x": z.x, 
+            "y": z.y,
+            "color": color 
+          });
+
+        this.#total = this.#total + 1;
+
+      }
+
+      // set for next #xp 
+      this.#xp = this.#xp + 1;
+      return points; 
+
+    }
+
+    getGridPoints(size=1) {
+      
+      
+      let points = [];
+      let num_points = 0;
+      let done = false;
+
+      // this.#yp = this.#yp % this.#pixels;
+      
+
+      while(this.#xp < this.#pixels) {
+
+        while(this.#yp < this.#pixels) {
+
+          // map the pixel into a number 
+          // in complex plane 
+          let z = this.plotter.mapXY(this.#xp, this.#yp);
+          let orbit = this.#transformer(z);
+          let color = this.getColor(orbit.n);
+          
+          points.push({
+            "x": z.x, 
+            "y": z.y,
+            "color": color 
+          });
+
+          // increase counters
+          num_points = num_points + 1;
+          this.#yp = this.#yp + 1;
+          this.#total = this.#total + 1;
+
+          // break innner loop
+          if(num_points >= size) {
+            done = true;
+            break;
+          }
+          
+        }
+
+        // y loop complete 
+        if(this.#yp == this.#pixels) {
+          // roll over y-value 
+          this.#yp = 0;
+          this.#xp = this.#xp + 1;
+        }
+
+        // collected points 
+        // break out of x loop 
+        if(done) {
+          break;
+        }
+
+      }
+      
+      return points;
+
+    }
+
+
     getColor(n) {
 
      
@@ -66,14 +164,14 @@ class Mandlebrot {
       }
 
       let index = this.#colors.length -1;
-      let index_size = this.#colorIndex.length;
+      let index_size = this.#colorsIndex.length;
 
       // typically we get elements in colors array 
       // than in the colors index array 
       // so we loop on colors indexes 
       for(let i =0; i < index_size; i = i +1) {
 
-        if(this.#colorIndex[i] >= n) {
+        if(this.#colorsIndex[i] >= n) {
           index = i;
           break;
         }
